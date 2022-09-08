d7ef538b336adf2a19c9b40bcb74f32a362ae41e
Dave Clydesdae <daveclydesdale@gmail.com>
2022-09-08 10:33:43 +0200

    initialState

---
+++ src/Redux/initialState.js
@@ -0,0 +1,153 @@
+export const initialState = {
+    reviews:[
+        {
+          reviewText: "Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum ",
+          movieData: {
+            id: "groundhog-day",
+            title: "Groundhog Day",
+            release_year: 1993,
+            genres: [
+                "Comedy"
+            ],
+            cast: [
+                {
+                    "id": "harold-ramis",
+                    "name": "Harold Ramis",
+                    "credits": [
+                        "Producer",
+                        "Screenplay",
+                        "Director",
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "bill-murray",
+                    "name": "Bill Murray",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "andie-macdowell",
+                    "name": "Andie MacDowell",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "chris-elliott",
+                    "name": "Chris Elliott",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "stephen-tobolowsky",
+                    "name": "Stephen Tobolowsky",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "danny-rubin",
+                    "name": "Danny Rubin",
+                    "credits": [
+                        "Screenplay"
+                    ]
+                },
+                {
+                    "id": "john-bailey",
+                    "name": "John Bailey",
+                    "credits": [
+                        "Cinematography"
+                    ]
+                },
+                {
+                    "id": "george-fenton",
+                    "name": "George Fenton",
+                    "credits": [
+                        "Music"
+                    ]
+                }
+            ],
+            image_url: "https://assets.mubi.com/images/film/2202/image-w1280.jpg?1513098953",
+            video_url: "https://trailers.mubi.com/2202/t-groundhog-day_en_us_1485217473.mp4"
+        }
+        },
+        {
+          reviewText: "Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum Hello world blñah blah blah words lorum stuff ipsum ",
+          movieData: {
+            id: "blue-velvet",
+            title: "Blue Velvet",
+            release_year: 1986,
+            genres: [
+                "Mystery",
+                "Drama",
+                "Cult"
+            ],
+            cast: [
+                {
+                    "id": "david-lynch",
+                    "name": "David Lynch",
+                    "credits": [
+                        "Screenplay",
+                        "Director"
+                    ]
+                },
+                {
+                    "id": "isabella-rossellini",
+                    "name": "Isabella Rossellini",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "kyle-maclachlan",
+                    "name": "Kyle MacLachlan",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "dennis-hopper",
+                    "name": "Dennis Hopper",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "laura-dern",
+                    "name": "Laura Dern",
+                    "credits": [
+                        "Cast"
+                    ]
+                },
+                {
+                    "id": "frederick-elmes",
+                    "name": "Frederick Elmes",
+                    "credits": [
+                        "Cinematography"
+                    ]
+                },
+                {
+                    "id": "angelo-badalamenti",
+                    "name": "Angelo Badalamenti",
+                    "credits": [
+                        "Music"
+                    ]
+                },
+                {
+                    "id": "duwayne-dunham",
+                    "name": "Duwayne Dunham",
+                    "credits": [
+                        "Editing"
+                    ]
+                }
+            ],
+            image_url: "https://assets.mubi.com/images/film/1309/image-w1280.jpg?1504764257",
+            video_url: "https://trailers.mubi.com/1309/t-blue-velvet-640w-700.m4v"
+        }
+        },
+      ],
+    movies: []
+}
